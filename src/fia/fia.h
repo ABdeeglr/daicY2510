0ab5b84bd2f8d7b8a77c3c0c61aba60b7e2fb8ec
abdeeglr <abdeeglr@icloud.com>
2025-10-24 23:23:35 +0800

    Set fia/fia.h some function definition

---
+++ src/fia/fia.h
@@ -0,0 +1,38 @@
+// src/fia/fia.h
+// Created At 2025-10-24:23:20:10
+// Author: ABdeeglr Ramsay
+
+#ifndef __FIA_H__
+#define __FIA_H__
+
+#define MACRO_X 1
+
+struct int_array {
+  int* body,
+  unsigned int size
+};
+
+typedef struct int_array* IntArray;
+typedef IntArray IA;
+
+
+
+/**
+ *   Int Array Builder and Destoryer
+ *
+ *
+ */
+IntArray IA_Builder(unsigned int length);
+IntArray IA_StackBuilder(unsigned int length);
+void IA_Destoryer(IA ia);
+
+/**
+ *    Utils Function For Int Array
+ */
+int IA_getLength(IA ia);
+int IA_setValue(IA ia, unsigned int position, int value);
+void IA_zeroes(IA ia);
+
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include "fia/fia.h"
 
 int main(const int argc, const char* argv[]) {
     printf("Hello world\n");
-
+    printf("%d\n", MACRO_X);
     return 0;
 }
