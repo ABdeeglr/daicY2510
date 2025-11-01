a28f2e672a8ccab7782fe915d2bb79a0fca25f27
abdeeglr <abdeeglr@icloud.com>
2025-11-01 15:47:53 +0800

    添加了一个简单的 debug 宏

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-#include "./fia/fia.h"
+#define DEBUG 1
+#include "debug.h"
 
 int main(const int argc, const char* argv[]) {
-    printf("Hello world\n");
-    printf("%d\n", MACRO_X);
+    
+    __DEBUG("Hello world!");
+    __WARNING("Here is %d warning.", 13);
+    __ERROR("And only %d Errors!", 0);
     return 0;
 }
