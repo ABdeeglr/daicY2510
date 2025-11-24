d62d204d69c899bcae579a31c2fe253d97283151
abdeeglr <abdeeglr@icloud.com>
2025-11-24 14:22:42 +0800

    update 打印数组的样式

--- src/main.c
+++ src/main.c
@@ -11,4 +11,20 @@ int main(const int argc, const char *argv[]) {
   int_array_print(arr);
   printf("Is ordered? %d %d\n", int_array_is_ordered_desc(arr), int_array_is_ordered_asc(arr));
   int_array_destroy(arr);  
+
+  arr = int_array_create(10);
+  int_array_fill_random_with_bound(arr, 30);
+  int_array_print(arr);
+
+  IA test = int_array_slice(arr, 2, 4);
+  int_array_print(test);
+
+  test = int_array_slice(arr, 0, 5);
+  int_array_print(test);
+
+  test = int_array_slice(arr, 5, 10);
+  int_array_print(test);
+  
+  int_array_destroy(test);
+  int_array_destroy(arr);
 }
