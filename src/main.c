4f4fa00781e29066c311dfda61356a63c98b5c03
abdeeglr <abdeeglr@icloud.com>
2025-11-27 21:43:56 +0800

    (fix) 微调可视化样式

--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
 #include "./analysis/fsort_analysis.h"
+#include "fia/fia.h"
+#include "fsort/fsort.h"
 
 int main(const int argc, const char *argv[]) {
 
   // sub_analysis_2_ordered_array_analysis(25, 5);
-  fsort_analyer(shell_sort, 3000000, 10);
+  // fsort_analyer(shell_sort, 3000000, 10);
+
+  IA arr = int_array_create(30);
+  int_array_fill_random_with_bound(arr, 500);
+  selection_sort(arr, NULL);
+  int_array_destroy(arr);
+
+  arr = int_array_create(30);
+  int_array_fill_random_with_bound(arr, 500);
+  insertion_sort(arr, NULL);
+  int_array_destroy(arr);
 
   return 0;
 }
