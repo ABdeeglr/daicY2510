3d1720dcbf28140db1b16a2780b3449549e05731
abdeeglr <abdeeglr@icloud.com>
2025-12-21 14:17:08 +0800

    Finish OOP style

--- src/unittest/newfia.c
+++ src/unittest/newfia.c
@@ -1,8 +1,12 @@
 #include "../fia/fia.h"
 
 int main(const int argc, const char** argv) {
-
-  FiaPackageTopMethod_UnitTest();
-
+  FiaPackageMethods Fia = FiaPackageTopMethod_Initialize();
+  iamResult res =  Fia.create(20);
+  Array arr;
+  if (res.status == SUCCESS) arr = res.value;
+  Fia.fillRandomWithBound(arr, Fia.getCapacity(arr) << 4);
+  Fia.display(arr);
+  Fia.destroy(arr);
   return 0;
 }
